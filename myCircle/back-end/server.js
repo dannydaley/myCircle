8eec1bde4763c688c5a9e66b870c57a743fd8eb7
DANNY DALEY <dd252935@falmouth.ac.uk>
2022-02-01 22:45:24 +0000

    Created a backend with some test endpoints, added an SQLite3 database for development, all currently working with populated dummy data

---
+++ myCircle/back-end/server.js
@@ -0,0 +1,124 @@
+//#region setup
+var express = require('express');
+const app = express();
+var dotenv = require('dotenv').config();
+
+//#endregion
+var sqlite3 = require('sqlite3').verbose();
+let SQLdatabase = new sqlite3.Database('./database/SQLdatabase.db');
+app.locals.SQLdatabase = SQLdatabase;
+
+
+//#region SQL DATABASE STUFF 
+//get the saved post information from external JSON file
+// mainly for keeping up to date when new post are created etc
+let postDataJSON = require("./database/posts.json");
+let userDataJSON = require("./database/users.json");
+
+const GET_USER_PROFILE_INFO = "SELECT name, joined, posts, profilePicture, aboutMe, pinnedPost FROM users WHERE name = ?" // SQL command
+const GET_ALL_POSTS = "SELECT * FROM `blog` ORDER BY id DESC"; // SQL command
+const GET_ALL_POSTS_BY_RECIPIENT = "SELECT * FROM `blog` WHERE recipient = ? ORDER BY id DESC"; // SQL command
+const GET_RECENT_POSTS = "SELECT * FROM blog WHERE recipient = ? ORDER BY id DESC LIMIT 5"; // SQL command
+const BLOG_DELETE_POST = "DELETE FROM `blog` WHERE title = ? AND id = ?"; // SQL command
+const GET_POSTS_BY_AUTHOR = "SELECT * FROM `blog` WHERE author = ? AND recipient = ? ORDER BY id DESC" // SQL command
+const GET_RECENT_POSTS_BY_AUTHOR = "SELECT * FROM blog WHERE author = ? AND recipient = ? ORDER BY id DESC LIMIT 5"; // SQL command
+const SQL_ADD_BLOG_POST = "INSERT INTO `blog` (author, title, image, content, link, date, recipient) VALUES(?,?,?,?,?,?,?)" // 
+const SQL_UPDATE_BLOG =  "UPDATE `blog` SET title = ?, image = ?, link = ?, author = ?, content = ? WHERE id = ?" //SQL command
+const SQL_UPDATE_USER_PROFILE = "UPDATE users SET profilePicture = ?, aboutMe = ? WHERE name = ?" // SQL command
+const SQL_UPDATE_USERS_PINNED_POST = "UPDATE users SET pinnedPost = ? WHERE name = ?" // SQL command
+const GET_ALL_USERS = "SELECT * FROM users"; // SQL command
+
+/* Database setup endpoint */
+app.get('/SQLDatabaseUserSetup', (req, res, next) => {
+  let SQLdatabase = req.app.locals.SQLdatabase;
+  //these queries must run one by one - dont try and delete and create tables at the same time.
+  SQLdatabase.serialize( () => {
+    //delete the table if it exists..
+    SQLdatabase.run('DROP TABLE IF EXISTS `users`');
+    //recreate the users table
+    // SQLdatabase.query('CREATE TABLE `users` (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255) UNIQUE COLLATE NOCASE, email varchar(255) UNIQUE, password varchar(255), passwordSalt varchar(512), posts int, joined varchar(255), profilePicture varchar(255), aboutMe text, pinnedPost INTEGER)');
+    SQLdatabase.run('CREATE TABLE `users` (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255) UNIQUE , email varchar(255) UNIQUE, password varchar(255), passwordSalt varchar(512), posts int, joined varchar(255), profilePicture varchar(255), aboutMe text, pinnedPost INTEGER)');
+    //create test rows
+    let rows = []    
+    for (let i = 0; i < userDataJSON.users.length; i++) {
+      rows[i] = [userDataJSON.users[i].name, userDataJSON.users[i].email, userDataJSON.users[i].password, userDataJSON.users[i].passwordSalt, userDataJSON.users[i].posts, userDataJSON.users[i].joined, userDataJSON.users[i].profilePicture,userDataJSON.users[i].aboutMe, userDataJSON.users[i].pinnedPost]
+    }
+    // add rows to database
+    rows.forEach( (row) => {
+      SQLdatabase.run('INSERT INTO `users` (name, email, password, passwordSalt, posts, joined, profilePicture, aboutMe, pinnedPost) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)', row);
+    });
+  })
+  //render success page
+  console.log("user table built");
+  res.send("user-db-done");
+})
+// set up blog table in database
+app.get('/SQLDatabaseBlogSetup', (req, res, next) => {
+  let SQLdatabase = req.app.locals.SQLdatabase;
+  //these queries must run one by one - dont try and delete and create tables at the same time.
+  SQLdatabase.serialize( () => {
+    //delete the table if it exists..
+    SQLdatabase.run('DROP TABLE IF EXISTS `blog`');
+    // create blog table
+    SQLdatabase.run('CREATE TABLE `blog` ( id INTEGER PRIMARY KEY AUTOINCREMENT, author varchar(255), title varchar(255), image varchar(255), content text, link varchar(255), date varchar(255), recipient varchar(255) )');
+    //create base rows
+    let rows = [];
+    //loop through posts.json to populate rows array
+    for (let i = 0; i < postDataJSON.entries.length; i++) {
+      rows[i] = [postDataJSON.entries[i].id, postDataJSON.entries[i].author, postDataJSON.entries[i].title, postDataJSON.entries[i].image, postDataJSON.entries[i].content, postDataJSON.entries[i].link, postDataJSON.entries[i].date, postDataJSON.entries[i].recipient]
+    }
+    // populate SQL command with rows array populated from posts.json
+    rows.forEach( (row) => {
+      // insert rows to table
+      SQLdatabase.run('INSERT INTO `blog` VALUES(?,?,?,?,?,?,?,?)', row);
+      // increment users post count according to author of currently processed post
+      
+    });
+  })
+  // render success page
+  console.log("blog table built");
+  res.send("blog-db-done");
+})
+
+/*==============================DEBUGGING AND TESTING ENDPOINTS========================*/
+/* GET all users */
+app.get('/getAllUsers', (req, res, next) => {
+  let SQLdatabase = req.app.locals.SQLdatabase;
+  // grab all user data
+  SQLdatabase.all(GET_ALL_USERS, [], (err, rows) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    }
+    res.send(rows);
+  })
+})
+
+/* GET all blog posts */
+app.get('/getAllPosts', (req, res, next) => {  
+  let SQLdatabase = req.app.locals.SQLdatabase;
+  // grab all posts
+  SQLdatabase.all(GET_ALL_POSTS, [], (err, rows) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    }    
+    res.json(rows);
+  })
+})
+/*========================END OF DEBUGGING AND TESTING ENDPOINTS========================*/
+
+//#endregion
+
+
+//#region endpoints
+app.get('/', (req, res) => {
+    console.log("port" + process.env.PORT + " '/' visited")
+    res.send("BACK END IS LISTENING ON PORT " + process.env.PORT)
+})
+
+
+// #endregion 
+
+app.listen(process.env.PORT)
+console.log("server.js running on port " + process.env.PORT)
