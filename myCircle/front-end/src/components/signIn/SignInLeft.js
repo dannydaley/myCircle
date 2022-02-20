072d70c3fd932855dbeaed23d89d6d98212444cf
DANNY DALEY <dd252935@falmouth.ac.uk>
2022-02-20 22:01:46 +0000

    Making sure all files are present in repo

--- myCircle/front-end/src/components/signIn/SignInLeft.js
+++ myCircle/front-end/src/components/signIn/SignInLeft.js
@@ -1,9 +1,11 @@
 import myCircle from '../../Images/myCircle.svg'
+import myCircleStamp from '../../Images/myCircleLogoStamp.svg'
 
 export default function SignInLeft() {
     return (
         <div style={{backgroundColor: '#217cd8', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <img src={myCircle} style={{width: '30vw'}}/>
+            {/* <img src={myCircleStamp} style={{width: '30vw'}} /> */}
         </div>
     )
 }
